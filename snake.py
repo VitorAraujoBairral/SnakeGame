5610bd090c1101268ec124e6faa5c22536ac705e
Vitor Araújo Bairral <vitorarabai@gmail.com>
2023-01-26 18:50:13 -0300

    tela

--- snake.py
+++ snake.py
@@ -1,6 +1,12 @@
-import turtle
+import turtle as tr
 
 #Criar a tela
+wn = tr.Screen()
+wn.setup(width=800, height=600)
+wn.bgcolor("gray")
+wn.title("Snake")
+wn.tracer(0)
+
 #Criar a cobra
 #Cria a movimentação da cobra
 #Cria o alimento e o crescimento da cobra
