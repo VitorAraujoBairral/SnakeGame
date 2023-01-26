c3c02826204a6504b2e0b67addc3958f7396cfbc
Vitor Araújo Bairral <vitorarabai@gmail.com>
2023-01-26 18:59:29 -0300

    instancia da cobra criada

--- snake.py
+++ snake.py
@@ -7,7 +7,25 @@ wn.bgcolor("gray")
 wn.title("Snake")
 wn.tracer(0)
 
+
 #Criar a cobra
+def instanciarCobra(parte):
+    parte.speed(0)
+    parte.shape("square")
+    parte.color("Green")
+    parte.penup()
+    parte.Aceleration = [0, 0]
+
+cabeca = tr.Turtle()
+cobra = [cabeca]
+
+instanciarCobra(cobra[0])
+
 #Cria a movimentação da cobra
 #Cria o alimento e o crescimento da cobra
 #Cria a mecânica de morte
+
+#loop principal do jogo:
+while True:
+
+    wn.update()
